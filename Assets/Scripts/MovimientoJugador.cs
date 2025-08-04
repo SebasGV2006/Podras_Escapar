78e5b68754ceadf9877d85d8292cbf2ee45491f7
Sebastian Grisales Vanegas <grisales1214@gmail.com>
2025-08-04 16:50:21 -0500

    Alpha 2.3 culminacion de las animaciones de movimientos del personaje de Baldo y sonido de caminata sobre la madera

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class MovimientoJugador : MonoBehaviour
 {
     public float speed = 5f;
+    AudioSource audioSource;
     public Vector2 direction;
+    private Animator animator;
 
     Rigidbody2D rigidBody;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        
     }
     
     private void FixedUpdate()
@@ -22,10 +27,34 @@ public class MovimientoJugador : MonoBehaviour
     private void Update()
     {
         Movement();
+        PlayMovementSound(); 
+        if (animator != null)
+        {
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Speed", direction.magnitude);
+        }
+        
     }
 
     private void Movement()
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
+
+    private void PlayMovementSound()
+    {
+        if (direction != Vector2.zero)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+    
 }
