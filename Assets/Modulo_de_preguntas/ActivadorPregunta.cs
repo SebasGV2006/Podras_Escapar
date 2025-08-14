94d67e174c3e0ed62e62fe2bc9434fe798c9f205
Sebastian Grisales Vanegas <grisales1214@gmail.com>
2025-08-14 17:32:46 -0500

    Alpha 3 agregando a funcionalidad de la activacion de preguntas

---
+++ Assets/Modulo_de_preguntas/ActivadorPregunta.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivadorPregunta : MonoBehaviour {
+    public GameObject Pregunta;
+
+    void Start() {
+        Pregunta.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.CompareTag("personaje")) {
+            Pregunta.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.CompareTag("personaje")) {
+            Pregunta.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Modulo_de_preguntas/ResponderPregunta.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResponderPregunta : MonoBehaviour
+{
+    public Text canva_puntos;
+    public int puntosPorRespuesta = 0;
+    public GameObject activadorPregunta;
+    public GameObject Pregunta;
+
+    public static int puntos;
+
+    void Start()
+    {
+        UpdatePuntosText(); // Actualizar el texto de los puntos al inicio
+    }
+
+    // Llamar a esta función pasando true si la respuesta es correcta, false si es incorrecta
+    public void Preguntas(bool esCorrecta)
+    {
+        if (esCorrecta)
+        {
+            puntos += puntosPorRespuesta;
+            StartCoroutine(res_correcta());
+        }
+        else
+        {
+            StartCoroutine(res_incorrecta());
+        }
+    }
+
+    IEnumerator res_correcta()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+        UpdatePuntosText();
+        yield return new WaitForSeconds(0.5f);
+        UpdatePuntosText();
+        
+        // Solo si es correcta, se eliminan
+        Destroy(Pregunta);
+        Destroy(activadorPregunta);
+        // Si quieres que deje de detectar inmediatamente antes de destruir:
+        // activadorPregunta.GetComponent<Collider2D>().enabled = false;
+    }
+
+    IEnumerator res_incorrecta()
+    {
+        // Si quieres un sonido diferente para respuesta incorrecta, ponlo aquí
+        yield return null; // No se destruye nada
+    }
+
+    void UpdatePuntosText()
+    {
+        canva_puntos.text = "puntos: " + puntos;
+    }
+
+    void ClearPuntosText()
+    {
+        canva_puntos.text = "";
+    }
+}
